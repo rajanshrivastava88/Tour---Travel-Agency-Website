0f4bd394cab31262ff427a5c6dd69393e1d8dfa2
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-03 20:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ const menuBtnIcon = menuBtn.querySelector("i");
 menuBtn.addEventListener("click", (e) => {
     navLinks.classList.toggle("open");
 
-    const isOpen = navLinks.classList.contains
-})
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
